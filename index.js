c93ef03276173995955ecb99bb88931b031b04c1
wang kun <SamWang@wangs-MacBook-Pro.local>
2017-03-18 00:05:45 +0800

    fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static('app'))
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/app/index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
